514e74b6625f05025a1910722c3b0ee272f3cbf1
ltara <ltara1385@gmail.com>
2026-01-23 20:13:29 +0330

    adding motion_block file

---
+++ motion_block.h
@@ -0,0 +1,44 @@
+
+#ifndef MOTION_BLOCK_H
+#define MOTION_BLOCK_H
+
+#include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfx.h>
+
+using namespace std;
+
+void moveSteps (int n)
+{
+    ;
+}
+
+void turnRight (int degree)
+{
+    ;
+}
+
+void turnLeft (int degree)
+{
+    ;
+}
+
+// can be used for "go to random position / mouse pointer" too
+void goToCoords (int x, int y)
+{
+    ;
+}
+
+void changeCoordsBy (int x, int y)
+{
+    ;
+}
+
+void pointInDirection (int degree)
+{
+    ;
+}
+
+#endif //MOTION_BLOCK_H
